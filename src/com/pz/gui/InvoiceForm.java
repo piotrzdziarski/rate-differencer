6cb15d3128831a5f9ede21fdba42d001627b826a
piotrzdziarski <pzdziarski@protonmail.com>
2025-06-19 08:15:35 +0200

    Forms' GUI

---
+++ src/com/pz/gui/InvoiceForm.java
@@ -0,0 +1,15 @@
+package com.pz.gui;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Insets;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class InvoiceForm extends Form {
+    public InvoiceForm(JPanel formsPanel, String label, String btnLabel) {
+        super(formsPanel, label, btnLabel);
+        
+    }
+}
